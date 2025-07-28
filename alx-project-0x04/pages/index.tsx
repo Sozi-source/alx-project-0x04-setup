d3a617ff2706c758c557a47e0b70b5bbd53ce4d8
Wilfred Osozi <osoziw@gmail.com>
2025-07-28 22:46:23 +0300

    added counter components

---
+++ alx-project-0x04/pages/index.tsx
@@ -0,0 +1,13 @@
+import Counter from "./counter-app"
+
+const Home: React.FC =()=>{
+
+
+  return(
+    <div>
+      <h2>Welcome to Home Page </h2>
+      <Counter />
+    </div>
+  )
+}
+export default Home;
